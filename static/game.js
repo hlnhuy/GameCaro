8baf5d47d88871b022180f2129deb7c04788f281
Lê Sĩ Trung <si9902612@gmail.com>
2026-01-09 16:52:00 +0700

    lan2

--- static/game.js
+++ static/game.js
@@ -1,17 +1,52 @@
 const socket = io();
-const board = document.getElementById("board");
+const boardDiv = document.getElementById("board");
 const size = 15;
 
-socket.on("connected", data => {
-    console.log(data.msg);
-});
+let myPlayer = null;
+let myTurn = false;
+let gameEnded = false;
 
-// Tạo bàn cờ 15x15
 for (let i = 0; i < size * size; i++) {
     const cell = document.createElement("div");
     cell.className = "cell";
+
     cell.onclick = () => {
-        cell.innerText = "X"; // test click
+        if (!myTurn || gameEnded) return;
+        const x = Math.floor(i / size);
+        const y = i % size;
+        socket.emit("make_move", { x, y });
     };
-    board.appendChild(cell);
+
+    boardDiv.appendChild(cell);
 }
+
+function startGame() {
+    socket.emit("start_game");
+}
+
+socket.on("assign_player", data => {
+    myPlayer = data.players[socket.id];
+    document.getElementById("start-popup").style.display = "none";
+    myTurn = (myPlayer === "X");
+});
+
+socket.on("update_board", data => {
+    const index = data.x * size + data.y;
+    boardDiv.children[index].innerText = data.player;
+});
+
+socket.on("turn_change", data => {
+    myTurn = (data.turn === myPlayer);
+});
+
+socket.on("game_over", () => {
+    gameEnded = true;
+});
+
+socket.on("reset_board", () => {
+    for (let cell of boardDiv.children) {
+        cell.innerText = "";
+    }
+    gameEnded = false;
+    myTurn = (myPlayer === "X");
+});
