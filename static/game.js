a60febc97410d2831e39370da0d9fab6426371b9
Lê Sĩ Trung <si9902612@gmail.com>
2026-01-12 23:36:43 +0700

    lan4

--- static/game.js
+++ static/game.js
@@ -1,45 +1,47 @@
 const socket = io();
+
 const boardDiv = document.getElementById("board");
 const startPopup = document.getElementById("start-popup");
+
 const size = 15;
-
-
 let myPlayer = null;
-
 
 for (let i = 0; i < size * size; i++) {
     const cell = document.createElement("div");
     cell.className = "cell";
-    
+
     cell.onclick = () => {
-       
         const x = Math.floor(i / size);
         const y = i % size;
-        console.log("Click tại tọa độ:", x, y);
+        
+        console.log(`Gửi nước đi: [${x}, ${y}]`);
+        socket.emit("make_move", { x, y });
     };
 
     boardDiv.appendChild(cell);
 }
 
 function startGame() {
-    console.log("Đang yêu cầu bắt đầu game...");
     socket.emit("start_game");
 }
 
-
 socket.on("waiting_start", () => {
     startPopup.style.display = "flex";
-    console.log("Đang đợi người chơi nhấn Start...");
 });
 
 socket.on("waiting_other_player", () => {
     startPopup.style.display = "flex";
- 
-    console.log("Đã sẵn sàng, đợi đối thủ kết nối...");
 });
 
 socket.on("assign_player", data => {
     myPlayer = data.players[socket.id];
     startPopup.style.display = "none";
-    alert("Bạn đã vào trận với vai trò: " + myPlayer);
+    console.log("Bạn là người chơi: " + myPlayer);
+});
+
+socket.on("update_board", data => {
+    const index = data.x * size + data.y;
+    if (boardDiv.children[index]) {
+        boardDiv.children[index].innerText = data.player;
+    }
 });
