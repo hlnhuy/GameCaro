6fb7f193fbff891acbf71d983cd05a036373394d
Lê Sĩ Trung <si9902612@gmail.com>
2026-01-16 20:33:08 +0700

    lan8

--- static/game.js
+++ static/game.js
@@ -3,20 +3,22 @@ const socket = io();
 const boardDiv = document.getElementById("board");
 const popup = document.getElementById("popup");
 const popupMsg = document.getElementById("popup-message");
+const replayBtn = document.getElementById("replay-btn");
 const startPopup = document.getElementById("start-popup");
 
 const size = 15;
 
 let myPlayer = null;
 let myTurn = false;
-let gameEnded = false; 
+let waitingReplay = false; 
+let gameEnded = false;
 
 for (let i = 0; i < size * size; i++) {
     const cell = document.createElement("div");
     cell.className = "cell";
 
     cell.onclick = () => {
-        if (!myTurn || gameEnded) return;
+        if (!myTurn || waitingReplay || gameEnded) return;
 
         const x = Math.floor(i / size);
         const y = i % size;
@@ -30,14 +32,24 @@ function startGame() {
     socket.emit("start_game");
 }
 
+socket.on("waiting_start", () => {
+    startPopup.style.display = "flex";
+});
+
+socket.on("waiting_other_player", () => {
+    startPopup.style.display = "flex";
+});
+
 socket.on("assign_player", data => {
     myPlayer = data.players[socket.id];
     startPopup.style.display = "none";
 
     if (myPlayer === "X") {
+        myTurn = true; 
         document.getElementById("player-left").innerText = "Bạn: X";
         document.getElementById("player-right").innerText = "Đối thủ: O";
     } else {
+        myTurn = false; 
         document.getElementById("player-left").innerText = "Bạn: O";
         document.getElementById("player-right").innerText = "Đối thủ: X";
     }
@@ -53,12 +65,20 @@ socket.on("turn_change", data => {
 });
 
 socket.on("game_over", data => {
-    gameEnded = true; 
+    gameEnded = true;
     myTurn = false;
+
     popup.classList.remove("hidden");
-    popupMsg.innerText = `${data.winner} đã giành chiến thắng!`;
+    popupMsg.innerText = `${data.winner} thắng!`;
+    replayBtn.style.display = "inline-block";
+
     document.getElementById("scoreX").innerText = data.score.X;
     document.getElementById("scoreO").innerText = data.score.O;
-    
-    console.log("Trận đấu kết thúc. Người thắng:", data.winner);
 });
+
+function requestReplay() {
+    waitingReplay = true;
+    replayBtn.style.display = "none";
+    popupMsg.innerText = "Đang đợi đối thủ...";
+    socket.emit("request_replay");
+}
